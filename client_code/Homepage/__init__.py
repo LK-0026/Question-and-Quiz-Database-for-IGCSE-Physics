208f53533042c82cf3d9f3a2c0c247fbc1d51ac5
leonardkurniadi2006@gmail.com <leonardkurniadi2006@gmail.com>
2023-11-29 03:55:12 +0000

    Edited Homepage and QuestionBank

--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -5,5 +5,7 @@ class Homepage(HomepageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    
     # Any code you write here will run before the form opens.
+  def button_question_click(self, **event_args):
+    open_form('QuestionBank',y_parameter="an_argument")
--- client_code/QuestionBank/__init__.py
+++ client_code/QuestionBank/__init__.py
@@ -7,3 +7,7 @@ class QuestionBank(QuestionBankTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_submit_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass

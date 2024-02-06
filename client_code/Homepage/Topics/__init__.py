cb9314ed6a7d7c86debd357f83f97c4e4c700574
leonardkurniadi2006@gmail.com <leonardkurniadi2006@gmail.com>
2024-02-06 02:03:32 +0000

    Created Homepage.Topics; Edited Homepage; Deleted QuestionBank

--- client_code/Homepage/Topics/__init__.py
+++ client_code/Homepage/Topics/__init__.py
@@ -1,11 +1,12 @@
-from ._anvil_designer import HomepageTemplate
+from ._anvil_designer import TopicsTemplate
 from anvil import *
 
-class Homepage(HomepageTemplate):
+class Topics(TopicsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
+
     # Any code you write here will run before the form opens.
-  def button_question_click(self, **event_args):
-    open_form('Homepage.Topics')
+
+  def back_button_click(self, **event_args):
+    open_form('Homepage')

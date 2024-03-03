7d17232d828095d52d91c118672e96444cc85edd
leonardkurniadi2006@gmail.com <leonardkurniadi2006@gmail.com>
2024-03-03 03:44:34 +0000

    Edited Homepage.Topics and Homepage.Topics.Questions

--- client_code/Homepage/Topics/Questions/__init__.py
+++ client_code/Homepage/Topics/Questions/__init__.py
@@ -10,6 +10,12 @@ class Questions(QuestionsTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.repeating_panel_1.items = app_tables.questions.search(topic = topicChosen)
+    self.subtopics = {'Motion, forces and energy': ['Motion','Energy, work and power'],
+                      'Thermal physics':[],
+                      'Waves':['Sound'],
+                      'Electricity and magnetism':[],
+                      'Nuclear physics':[],
+                      'Space physics':[]}
     # Any code you write here will run before the form opens.
   def back_button_click(self, **event_args):
     open_form('Homepage.Topics')

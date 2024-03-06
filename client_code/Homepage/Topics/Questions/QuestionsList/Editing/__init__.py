c2775f81e5158c9e2f0b79fc56747ec53c2df7f0
leonardkurniadi2006@gmail.com <leonardkurniadi2006@gmail.com>
2024-03-06 07:54:38 +0000

    Edited Homepage.Topics.Questions.QuestionsList.Editing

--- client_code/Homepage/Topics/Questions/QuestionsList/Editing/__init__.py
+++ client_code/Homepage/Topics/Questions/QuestionsList/Editing/__init__.py
@@ -8,13 +8,15 @@ from anvil.tables import app_tables
 class Editing(EditingTemplate):
   def __init__(self, topic, subtopic, questionText, image, option1, option2, option3, option4, correctAnswer, **properties):
     # Set Form properties and Data Bindings.
-    self.init_components
+    self.init_components(**properties)
+    self.topicChosen = topic
+    self.correctAnswer = correctAnswer
     # Any code you write here will run before the form opens
     self.label_topic.text = topic
     self.subtopics = anvil.server.call('getSubtopics')
     self.drop_down_subtopics.items = [""] + self.subtopics[topic]
     self.drop_down_subtopics.selected_value = subtopic
-    self.text_area_questionText = questionText
+    self.text_area_questionText.text = questionText
     if image != None:
       self.image_question.source = self.file_loader_image.file
       self.image_question.visible = True
@@ -23,11 +25,12 @@ class Editing(EditingTemplate):
     self.text_area_option2.text = option2
     self.text_area_option3.text = option3
     self.text_area_option4.text = option4
-    if correctAnswer == option1:
+    
+    if correctAnswer == "option1":
       self.radio_button_option1.selected = True
-    elif correctAnswer == option2:
+    elif correctAnswer == "option2":
       self.radio_button_option2.selected = True
-    elif correctAnswer == option3:
+    elif correctAnswer == "option3":
       self.radio_button_option3.selected = True 
     else:
       self.radio_button_option4.selected = True 
@@ -67,21 +70,9 @@ class Editing(EditingTemplate):
 
     missingFields.strip()
     if missingFields != "":
-      alert("The following field(s) must be filled before a question can be added:\n" + missingFields)
-    else:
-      app_tables.questions.add_row(
-        topic = self.topicChosen,
-        subtopic = self.drop_down_subtopics.selected_value,
-        text = self.text_area_questionText.text,
-        image = self.file_loader_image.file,
-        option1 = self.text_area_option1.text,
-        option2 = self.text_area_option2.text,
-        option3 = self.text_area_option3.text,
-        option4 = self.text_area_option4.text,
-        correctAnswer = self.correctAnswer,
-        isUsed = False)
-      alert("Question has succesfully been added.")
-      open_form('Homepage.Topics.Questions', topicChosen = self.topic)
+      alert("The following field(s) must be filled before a question can be edited:\n" + missingFields)
+    #else:
+      
       
   def back_button_click(self, **event_args):
     """This method is called when the button is clicked"""

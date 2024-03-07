6dfa297819e3fb99aaf6e9b7fbb14e92a5a1af62
leonardkurniadi2006@gmail.com <leonardkurniadi2006@gmail.com>
2024-03-07 14:04:45 +0000

    Edited Homepage and Homepage.Topics

--- client_code/Homepage/Topics/__init__.py
+++ client_code/Homepage/Topics/__init__.py
@@ -8,18 +8,19 @@ from Questions import Questions
 
 class Topics(TopicsTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    option = None
-    # Any code you write here will run before the form opens.
+    #List which contains all the topics in the Question Bank
     self.topics = ["Motion, forces and energy", "Thermal physics", "Waves", "Electricity and magnetism","Nuclear physics","Space physics"]
+    #The drop down box contains
     self.topics_list.items = self.topics
+
+  #Function that opens the previous form
   def back_button_click(self, **event_args):
     open_form('Homepage')
 
+  
   def enter_click(self, **event_args):
-    option = self.topics_list.selected_value
-    open_form('Homepage.Topics.Questions', topicChosen=option)
+    open_form('Homepage.Topics.Questions', topicChosen = self.topics_list.selected_value)
     
     
 
--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -7,9 +7,8 @@ import anvil.server
 
 class Homepage(HomepageTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
     
-    # Any code you write here will run before the form opens.
+  #Open the Question Bank section of the program
   def button_question_click(self, **event_args):
     open_form('Homepage.Topics')

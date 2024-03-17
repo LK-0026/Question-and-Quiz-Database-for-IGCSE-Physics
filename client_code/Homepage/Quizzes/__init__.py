cb3ab07fa40c7e70e3f8d96661c5d04a2dc7dabc
leonardkurniadi2006@gmail.com <leonardkurniadi2006@gmail.com>
2024-03-17 06:24:41 +0000

    Edited Homepage.Quizzes

--- client_code/Homepage/Quizzes/__init__.py
+++ client_code/Homepage/Quizzes/__init__.py
@@ -9,7 +9,7 @@ class Quizzes(QuizzesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.items = app_tables.quizzes.search()
+    self.repeating_panel_quizzesList.items = app_tables.quizzes.search()
 
   #Goes back to the previous form
   def back_button_click(self, **event_args):
@@ -17,4 +17,10 @@ class Quizzes(QuizzesTemplate):
 
   #Searches for quizzes 
   def text_box_searchQuiz_pressed_enter(self, **event_args):
+    rowsFound = []
+    for row in app_tables.quizzes.search():
+      if self.text_box_searchQuiz.text.lower() in row['quizName'].lower():
+        rowsFound.append(row)
     
+    self.repeating_panel_quizzesList.items = rowsFound
+        

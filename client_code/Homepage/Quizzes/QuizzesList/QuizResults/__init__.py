948c933726dea8b769b23387bc10345012986f4c
leonardkurniadi2006@gmail.com <leonardkurniadi2006@gmail.com>
2024-03-26 15:28:10 +0000

    Created Homepage.Quizzes.QuizzesList.QuizResults; Deleted Homepage.Quizzes.QuizzesList.Form1

--- client_code/Homepage/Quizzes/QuizzesList/QuizResults/__init__.py
+++ client_code/Homepage/Quizzes/QuizzesList/QuizResults/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import QuizResultsTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -7,7 +7,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class QuizResults(QuizResultsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)

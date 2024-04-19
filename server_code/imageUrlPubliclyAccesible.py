391ee1e549fbfa74904c21d9d55e05b3e1fc6a71
leonardkurniadi2006@gmail.com <leonardkurniadi2006@gmail.com>
2024-04-19 11:24:19 +0000

    Edited Homepage.Quizzes.QuizzesList and imageUrlPubliclyAccesible

--- server_code/imageUrlPubliclyAccesible.py
+++ server_code/imageUrlPubliclyAccesible.py
@@ -5,7 +5,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-@anvil.server.http_endpoint('/get_image')
-def getImage(rowID):
-  questionImage = app_tables.questions.get_by_id(rowID)['image']
-  return questionImage
+@anvil.server.http_endpoint('/image/:row_id')
+def get_image(row_id, **p):
+  row = app_tables.questions.get_by_id(row_id)
+  return row['image']
